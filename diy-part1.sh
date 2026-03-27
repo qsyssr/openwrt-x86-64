980605a60ecc4cc9fa3bc1bd719c38017ece3b53
qsyssr <46160354+qsyssr@users.noreply.github.com>
2026-03-27 14:36:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,16 +19,13 @@
 
 #!/bin/bash
 
-# 1. 清理可能存在的重复包 (防止编译因同名包冲突失败)
+# 1. 清理可能存在的重复包 (防止编译冲突)
 rm -rf feeds/luci/applications/luci-app-lucky
-rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-easytier
+rm -rf feeds/luci/applications/luci-app-adguardhome
 
-# 3. 克隆插件到 package 目录 (确保路径正确)
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git package/openclash
-git clone https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-
-# 4. 克隆屏幕驱动和相关工具 (移动到 package 目录下)
-# git clone https://github.com/zzzz0317/xgp-v3-screen package/xgp-v3-screen
-# git clone https://github.com/zzzz0317/kmod-fb-tft-gc9307 package/kmod-fb-tft-gc9307
+# 2. 克隆插件 (增加 --depth=1 加速编译)
+echo "Cloning custom packages..."
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
